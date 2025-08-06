0cdb7944d23c38e700f11a8c3f47788f7eb76603
othiagotenorio2 <thiago.tenorio@cotefacil.com>
2025-08-06 16:23:06 -0300

    criação de metodo, classe ClienteService

--- src/main/java/com/barbershopteno/barbershop/service/ClienteService.java
+++ src/main/java/com/barbershopteno/barbershop/service/ClienteService.java
@@ -5,6 +5,8 @@ import com.barbershopteno.barbershop.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClienteService {
 
@@ -17,6 +19,19 @@ public class ClienteService {
 
     public Cliente buscarPorId(Long id){
         return repository.findById(id).orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
+    }
+
+    public List<Cliente> listarTodos() {
+        return repository.findAll();
+    }
+
+    public Cliente atualizar(Long id, Cliente clienteAtualizado) {
+        Cliente cliente = buscarPorId(id);
+        cliente.setNome(clienteAtualizado.getNome());
+        cliente.setEmail(clienteAtualizado.getEmail());
+        cliente.setTelefone(clienteAtualizado.getTelefone());
+        cliente.setCpf(clienteAtualizado.getCpf());
+        return repository.save(cliente);
     }
 
 }
